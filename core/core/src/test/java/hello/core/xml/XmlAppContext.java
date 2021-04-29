d0d174fe12ba71bc83944cf5e86a97bb88a986f0
Yaerhee <yaerhee@naver.com>
2021-04-29 19:08:53 +0900

    Spring: connect, test with appConfig.xml

---
+++ core/core/src/test/java/hello/core/xml/XmlAppContext.java
@@ -0,0 +1,20 @@
+package hello.core.xml;
+
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class XmlAppContext {
+
+    @Test
+    void xmlAppContext() {
+        //부모 class인 ApplicationContext를 기반으로 객체 생성
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+
+}
