ef902a869058ece51834090bfd71d36c8f767818
Yaerhee <yaerhee@naver.com>
2021-04-30 14:43:18 +0900

    Singleton pattern: pure DI container Test

---
+++ core/core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,32 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스프링 없는 순수 DI 컨테이너")
+    void pureContainer() {
+        AppConfig appConfig = new AppConfig();
+        //1. 조회: 호출할 때 마다 객체를 생성
+        MemberService memberService1 = appConfig.memberService();
+
+        //1-2. 조회: 호출할 때 마다 객체를 생성
+        MemberService memberService2 = appConfig.memberService();
+
+        //참조 값이 다른 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        //memberService 1 != memberService2
+        assertThat(memberService1).isNotSameAs(memberService2);
+        //호출 할 때마다 객체를 새로 생성함 -> 총 4개 생성된 것과 같음
+    }
+}
