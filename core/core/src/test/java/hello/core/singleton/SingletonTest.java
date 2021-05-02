67962894648778deab99198a40253bf10786b887
Yaerhee <yaerhee@naver.com>
2021-05-02 22:53:36 +0900

    Singleton pattern with Spring container

--- core/core/src/test/java/hello/core/singleton/SingletonTest.java
+++ core/core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -2,9 +2,10 @@ package hello.core.singleton;
 
 import hello.core.AppConfig;
 import hello.core.member.MemberService;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -44,6 +45,24 @@ public class SingletonTest {
         assertThat(singletonService1).isSameAs(singletonService2);
         //.issameAs? ==과 같음
         //.equals? 내용을 비교 
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")//스프링 컨테이너 적용 및 연동
+    void springContainer() { //.vs pureContainer()
+        //AppConfig appConfig = new AppConfig();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        
+        //스프링 컨테이너를 활용할 경우 new를 사용하여 객체 생성을 하지 않아도 됨
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        //참조 값이 다른 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        //memberService 1 != memberService2
+        assertThat(memberService1).isSameAs(memberService2);
     }
 
 }
