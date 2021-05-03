bf19f982f8c9466681ed42c7c194b7c700df5b9c
Yaerhee <yaerhee@naver.com>
2021-05-03 23:55:41 +0900

    Singleton: exception - shared field

---
+++ core/core/src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,40 @@
+package hello.core.singleton;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatefulServiceTest {
+
+    @Test
+    void statefulServiceSingleton() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        //ThreadA: A사용자 10000원 주문
+        statefulService1.order("userA", 10000);
+        //ThreadB: B사용자 20000원 주문
+        statefulService1.order("userB", 20000);
+
+        //ThreadA: 사용자A 주문 금액 조회
+        int price = statefulService1.getPrice();
+        //ThreadA: 사용자A는 10000원을 기대 했지만, 기대와 다르게 20000원 출력(!?)
+        System.out.println("price = " + price);//userB와 20000원의 데이터가 입력되었기 때문에 20000원이 출력된 것
+
+        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+    }
+
+    static class TestConfig {
+
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+
+}
